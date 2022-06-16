fd38f3f5b19fc9e3af97d6a583a2b0a4aadcb4ec
Ken Ingle <ken@keningle.net>
2022-06-16 10:17:54 -0600

    Allow user to enter size

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const startButton = document.getElementById('startButton');
 const scoreDisplay = document.getElementById('scoreDisplay');
 const messageDisplay = document.getElementById('messageDisplay');
 const gridDiv = document.getElementById('grid');
+const sizeInput = document.getElementById('sizeInput');
 
-const width = 20;
+let width = 20;
 const MOVE_RIGHT = 1;
 const MOVE_LEFT = -1;
-const MOVE_UP = -width;
-const MOVE_DOWN = width;
+let MOVE_UP = -width;
+let MOVE_DOWN = width;
 
 let gridSquares = [];
 let snake = [];
@@ -15,11 +16,11 @@ let currentDirection;
 let intervalTime = 500;
 let tickInterval;
 
-for (let i = 0; i < width * width; i++) {
-  const newDiv = document.createElement('div');
-  gridDiv.appendChild(newDiv);
-  gridSquares.push(newDiv);
-}
+// for (let i = 0; i < width * width; i++) {
+//   const newDiv = document.createElement('div');
+//   gridDiv.appendChild(newDiv);
+//   gridSquares.push(newDiv);
+// }
 
 function endGame(message) {
   messageDisplay.innerText = message;
@@ -95,10 +96,34 @@ function onGameTick() {
   moveSnake();
 }
 
+function setGridSize(size) {
+  while (gridDiv.firstChild) {
+    gridDiv.removeChild(gridDiv.firstChild);
+  }
+
+  gridSquares = [];
+  width = size;
+  MOVE_UP = -1 * width;
+  MOVE_DOWN = width;
+  let pixelSize = 600 / size;
+  for (let i = 0; i < width * width; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.style.width = pixelSize + "px";
+    newDiv.style.height = pixelSize + "px";
+    gridDiv.appendChild(newDiv);
+    gridSquares.push(newDiv);
+  }
+}
+
 function startGame() {
-  snake.forEach(index => gridSquares[index].classList.remove('snake'));
+  setGridSize(parseInt(sizeInput.value));
+  
+  snake.forEach(index => {
+    gridSquares[index].classList.remove('snake');
+  });
+  
   gridSquares.forEach(square => square.classList.remove('apple'));
-  snake = [66, 65, 64, 63, 62, 61, 60];
+  snake = [62, 61, 60];
   currentDirection = MOVE_RIGHT;
   snake.forEach(index => {
     gridSquares[index].classList.add('snake');
